cd474ac7297c047d051d5294941a077b78d726fe
LorenzoDalmaau <lorenzo.poderoso.7e6@itb.cat>
2024-05-10 16:20:24 +0200

    Launcher icon and splash screen

--- lib/src/ui/screens/main_holder/notices.dart
+++ lib/src/ui/screens/main_holder/notices.dart
@@ -1,47 +1,47 @@
-// import 'package:app_merchants_association/src/config/app_colors.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-// import 'package:webview_flutter/webview_flutter.dart';
-//
-// class Notices extends StatefulWidget {
-//   const Notices({super.key});
-//
-//   @override
-//   State<Notices> createState() => _NoticesState();
-// }
-//
-// class _NoticesState extends State<Notices> {
-//
-//   WebViewController controller = WebViewController()
-//     ..setJavaScriptMode(JavaScriptMode.unrestricted)
-//     ..setBackgroundColor(const Color(0x00000000))
-//     ..setNavigationDelegate(
-//       NavigationDelegate(
-//         onProgress: (int progress) {
-//
-//         },
-//         onPageStarted: (String url) {},
-//         onPageFinished: (String url) {},
-//         onWebResourceError: (WebResourceError error) {},
-//         onNavigationRequest: (NavigationRequest request) {
-//           if (request.url.startsWith('https://www.youtube.com/')) {
-//             return NavigationDecision.prevent;
-//           }
-//           return NavigationDecision.navigate;
-//         },
-//       ),
-//     )
-//     ..loadRequest(Uri.parse('https://elpais.com/#'));
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(
-//           AppLocalizations.of(context)!.news,
-//         ),
-//       ),
-//       body: WebViewWidget(controller: controller),
-//     );
-//   }
-// }
+import 'package:app_merchants_association/src/config/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class Notices extends StatefulWidget {
+  const Notices({super.key});
+
+  @override
+  State<Notices> createState() => _NoticesState();
+}
+
+class _NoticesState extends State<Notices> {
+
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..setBackgroundColor(const Color(0x00000000))
+    ..setNavigationDelegate(
+      NavigationDelegate(
+        onProgress: (int progress) {
+
+        },
+        onPageStarted: (String url) {},
+        onPageFinished: (String url) {},
+        onWebResourceError: (WebResourceError error) {},
+        onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://www.youtube.com/')) {
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        },
+      ),
+    )
+    ..loadRequest(Uri.parse('https://elpais.com/#'));
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          AppLocalizations.of(context)!.news,
+        ),
+      ),
+      body: WebViewWidget(controller: controller),
+    );
+  }
+}
